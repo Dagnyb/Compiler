5ea0b995d45734f1b4f10b8807a49b22af6a48f7
Dagnyb <dagny18@hotmail.com>
2017-02-05 18:09:25 +0000

    methods staging

--- Compiler/src/Lexer.java
+++ Compiler/src/Lexer.java
@@ -1,4 +1,27 @@
 
 public class Lexer {
-
+	//Statements -> Statement ; Statements | end
+	void Statements(){
+		
+	}
+	
+	//Statement -> id = Expr | print id
+	void Statement(){
+		
+	}
+	
+	//Expr -> Term | Term + Expr | Term – Expr
+	void Expr(){
+		
+	}
+	
+	//Term -> Factor | Factor * Term
+	void Term(){
+		
+	}
+	
+	//Factor -> int | id | ( Expr )
+	void Factor(){
+		
+	}
 }
