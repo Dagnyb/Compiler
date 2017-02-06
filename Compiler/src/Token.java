2ae3e3a36e9344fcdef3ab18318844179cadcba6
Halla Sigrún <hallas14@ru.is>
2017-02-06 15:00:32 +0000

    Setti verkefnalýsingu við ogþað sem segir að eigai að gera

--- Compiler/src/Token.java
+++ Compiler/src/Token.java
@@ -1,4 +1,16 @@
+/*
+ Implement the class Token, which contains both a lexeme and a token code:
+ String lexeme; 
+ TokenCode tCode;
+ where TokenCode is:
+ enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB, MULT, LPAREN, RPAREN, PRINT, END, ERROR }
+ Note: See explanation for ERROR in 2
+*/
+
 
 public class Token {
+	
+	public String lexeme;
+	public TokenCode tCode;
 
 }
