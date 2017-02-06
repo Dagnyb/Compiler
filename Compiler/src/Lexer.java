3b0983c06e9e02651c74257c5758ae3137b30a2d
Dagnyb <dagny18@hotmail.com>
2017-02-06 15:08:03 +0000

    Clean Lexer

--- Compiler/src/Lexer.java
+++ Compiler/src/Lexer.java
@@ -3,31 +3,6 @@ public class Lexer {
 	// next char
 	char lookahead;
 	
-	//Statements -> Statement ; Statements | end
-	void Statements(){
-		
-	}
-	
-	//Statement -> id = Expr | print id
-	void Statement(){
-		
-	}
-	
-	//Expr -> Term | Term + Expr | Term – Expr
-	void Expr(){
-		
-	}
-	
-	//Term -> Factor | Factor * Term
-	void Term(){
-		
-	}
-	
-	//Factor -> int | id | ( Expr )
-	void Factor(){
-		
-	}
-	
 	 public static void main(String[] args) {
 		 
 	 }
